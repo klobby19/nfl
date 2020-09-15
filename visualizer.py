dd817a53c61f03c12d160a2efa24ec6af890dc6a
klobby19 <klobby19@gmail.com>
2020-09-15 15:06:02 -0700

    visualize

--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy
+plt.style.use('seaborn-deep')
 
-data = pd.read_csv('average_age.txt')
-df = pd.DataFrame(data)
+def remove_spaces(list):
+    result = []
+    for item in list:
+        if item != '':
+            result.append(item)
+    return result
 
-plt.scatter(df['team'],df['age'])
-plt.show()
+def average_age_visualize():
+    data = pd.read_csv('average_age.txt')
+    df = pd.DataFrame(data)
+
+    fig = plt.figure(figsize=(20,15))
+    plt.title('Average age of each NFL team')
+    for index in df.index:
+        row = str(df.iloc[index]).split(' ')
+        row = remove_spaces(row)
+        team = row[1].replace('age', '').replace(' ','')
+        age = row[2].replace('Name:', '')
+        plt.scatter(team, float(age), c=numpy.random.rand(3,))
+
+    plt.show()
+
+
